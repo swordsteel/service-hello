c9af6df3f8f1451b1626987b03f95e123c2c06ea
Mr Swordsteel <swordsteel@gmail.com>
2024-03-05 15:24:42 +0100

    enable configuration and properties scan in Application

--- src/main/kotlin/ltd/lulz/service/hello/Application.kt
+++ src/main/kotlin/ltd/lulz/service/hello/Application.kt
@@ -1,9 +1,13 @@
 package ltd.lulz.service.hello
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
+@EnableConfigurationProperties
 class Application
 
 fun main(vararg args: String) {
