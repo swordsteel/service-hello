e6a54efed8dcfe2919c607dd405d2798aa74f037
Mr Swordsteel <swordsteel@gmail.com>
2024-03-07 14:59:56 +0100

    change from spring-boot-starter-web to spring-boot-starter-webflux add reactor-test

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,8 +11,9 @@ plugins {
 dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
 
+    testImplementation("io.projectreactor:reactor-test")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
