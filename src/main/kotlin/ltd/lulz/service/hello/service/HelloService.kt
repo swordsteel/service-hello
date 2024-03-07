6164596374f99c4c388b50a714d55a25fe9b274c
Mr Swordsteel <swordsteel@gmail.com>
2024-03-07 15:58:45 +0100

    add HelloService

---
+++ src/main/kotlin/ltd/lulz/service/hello/service/HelloService.kt
@@ -0,0 +1,12 @@
+package ltd.lulz.service.hello.service
+
+import ltd.lulz.service.hello.configuration.HelloConfiguration
+import org.springframework.stereotype.Service
+
+@Service
+class HelloService(
+    val configuration: HelloConfiguration,
+) {
+
+    suspend fun getGrating(): String = "Hello there, This is ${configuration.name} v${configuration.version}!!!"
+}
